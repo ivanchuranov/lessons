06f8c9dec39cc5d98c7776abf30fdcf37dd53b68
ivanchuranov <ivanchuranov@icloud.com>
2022-09-17 12:15:22 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,17 @@
+nickname = "Sagge"
+speed = 0.2
+healh = 100
+info = f"Никнейм: {nickname} Speed: {speed}"
+print('Hello, Ivan')
+print(nickname)
+print('your nickname',nickname, "- Minecraft")
+print(f'your nickname{nickname} - minecraft')
+
+login = input('введите логин: ')
+print(login)
+
+h = int(input('введите возвраст'))
+next_age = int(h) + 1
+print(f"в следующем году вам будет {next_age}")
+natural_numbers = 1, 2, 3
+
